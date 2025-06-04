93f617589d2a9c1e70822a169d4e1daf2a22bac9
Armaan Srivastava <63304841+Armaansri2000@users.noreply.github.com>
2025-06-05 00:55:04 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true // 👈 this will open the default browser automatically
+  }
 })
